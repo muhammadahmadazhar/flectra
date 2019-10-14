fda29c491cda81ce9cac9ff2f57da005cce35987
muhammadahmadazhar <muhammadahmadazhar16@gmail.com>
2019-10-14 16:08:17 +0500

    changing

--- library/addons/library_book/models/library_book.py
+++ library/addons/library_book/models/library_book.py
@@ -2,7 +2,10 @@ from flectra import models, fields
 
 class LibraryBook(models.Model):
     _name = 'library_book.library_book'
-    name = fields.Char('Title', required=True)
+    name = fields.Char('name', required=True)
+    father_name=fields.Char('Father Name' )
+    agee=fields.Date('age')
+    description = fields.Char('desc')
     date_release = fields.Date('Release Date')
     author_ids = fields.Many2many(
         'res.partner',
